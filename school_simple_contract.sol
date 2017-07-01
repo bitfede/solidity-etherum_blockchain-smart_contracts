9f30a44aab2f98a25b541f3f1f2c5d65e276c96f
Fede Defaveri <f.g.defaveri@gmail.com>
2017-07-01 16:29:45 +0200

    added set registration fee function

--- school_simple_contract.sol
+++ school_simple_contract.sol
@@ -8,6 +8,8 @@ contract School {
 
 	}
 
+	uint256 public registrationFee;
+
 	mapping(address => Student) public myStudents;
 
 	function register (string _myName, uint myAge) {
@@ -17,10 +19,14 @@ contract School {
 			studentName: _myName,
 			studentAge: _myAge,
 			active: true
-			
+
 			});
 	}
 
+	function setFee(uint256 _newRegistrationFee) {
 
+		registrationFee = _newRegistrationFee;
+		
+	}
 
 }
