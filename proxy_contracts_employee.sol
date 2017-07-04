b946ecb87ab1dd51463bb642bded5480882283bb
Fede Defaveri <f.g.defaveri@gmail.com>
2017-07-04 14:02:11 +0200

    create the employee contract

--- proxy_contracts_employee.sol
+++ proxy_contracts_employee.sol
@@ -1,3 +1,26 @@
-contract child {
-	
+// AUTHOR: 		Federico Giorgio De Faveri
+// DATE: 			Jul 4th 2017
+// PURPOSE: 	This smart contract aims to introduce in a very simple way the notion of PROXY CONTRACTS in Solidity.
+//						It will be accomplished by creating an `employee` contract, which can be modified by an `employer` contract
+//						
+// LANGUAGE:  Solidity
+
+contract Employee {
+
+	//variables
+	string public employeeName;
+	uint public employeeAge;
+	string public employeeRole;
+
+	//constructor
+	function Employee(string _name , uint _age) {
+		employeeName = _name;
+		employeeAge = _age;
+	}
+
+	function setEmployeeRole(string _role) {
+		employeeRole = _role;
+	}
+
 }
+
